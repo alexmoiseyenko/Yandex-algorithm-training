4dc00106b77fe45ce286af09a9fb05dcbd0924e6
Alexey Moiseenko <alexey.moiseyenko@gmail.com>
2023-10-10 16:27:41 +0200

    feat: intercator

---
+++ Тренировки 2.0/Дивизион В/A. Interactor/index.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+fs.readFile("input1.txt", "utf8", (err, data) => {
+    const arr = data.trim().split('\n').map(Number);
+
+    const taskCompletionCode  = arr[0];
+    const interactor = arr[1];
+    const checker = arr[2];
+
+    let res;
+
+    switch (interactor) {
+        case 0:
+            if (taskCompletionCode === 0) {
+                res = checker;
+            } else {
+                res = 3;
+            }
+            break;
+        case 1:
+            res = checker;
+            break;
+        case 4:
+            if (taskCompletionCode === 0) {
+                res = 4;
+            } else {
+                res = 3;
+            }
+            break;
+        case 6:
+            res = 0;
+            break;
+        case 7:
+            res = 1;
+            break;
+        default:
+            res = interactor;
+    }
+
+    fs.writeFile("output.txt", res.toString(), () => {});
+});
