0498e3b14f61ff4e54a496bbfd7fed93d946c357
Alexey Moiseenko <alexey.moiseyenko@gmail.com>
2023-10-10 20:56:29 +0200

    feat: Koltsevaya line

---
+++ Тренировки 2.0/Дивизион В/B. Кольцевая линия метро/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+fs.readFile("input2.txt", "utf8", (err, data) => {
+    const arr = data.trim().split(' ').map(Number);
+
+    const numberOfAllStations = arr[0];
+    const startStationNum = arr[1];
+    const endStationNum = arr[2];
+
+    const clockwiseStationsNum = Math.abs(endStationNum - startStationNum) - 1;
+    const counterclockwiseStationsNum = startStationNum > endStationNum ? 
+        numberOfAllStations - startStationNum + endStationNum - 1 :
+        startStationNum - 1 + numberOfAllStations - endStationNum;
+
+    const res = Math.min(clockwiseStationsNum, counterclockwiseStationsNum);
+
+    console.log(res)
+
+    fs.writeFile("output.txt", res.toString(), () => {});
+});
